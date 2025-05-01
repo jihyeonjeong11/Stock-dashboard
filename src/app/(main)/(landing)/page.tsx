d364c139967b3a710ec0900439aa343af926d7f3
jihyeonjeong11 <wjdwlgus11@gmail.com>
2025-05-01 18:17:11 +0900

    need template update..

--- src/app/(main)/(landing)/page.tsx
+++ src/app/(main)/(landing)/page.tsx
@@ -1,7 +1,6 @@
 import { appConfig } from "@/app-config";
 import { ComingSoon } from "@/app/(main)/(coming-soon)/coming-soon";
 import { HeroSection } from "./_sections/hero";
-import PricingSection from "./_sections/pricing";
 
 export default function Home() {
   if (appConfig.mode === "comingSoon") {
@@ -11,7 +10,6 @@ export default function Home() {
   return (
     <div>
       <HeroSection />
-      <PricingSection />
     </div>
   );
 }

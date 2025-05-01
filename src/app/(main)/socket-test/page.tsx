470b839ddc070bc1f5e317293ea2cf3ef76f1f92
jihyeonjeong11 <wjdwlgus11@gmail.com>
2025-05-01 19:01:03 +0900

    Forgot

--- src/app/(main)/socket-test/page.tsx
+++ src/app/(main)/socket-test/page.tsx
@@ -5,9 +5,11 @@ import { env } from "@/env";
 import { pageTitleStyles } from "@/styles/common";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function SocketTestPage() {
+  const [message, setMessage] = useState("");
+  const [timeStamp, setTimeStamp] = useState("");
   useEffect(() => {
     // console.log(socket);
     // if (socket.connected) {
@@ -16,8 +18,6 @@ export default function SocketTestPage() {
     // try 1: use vanilla Websocket
     const socket = new WebSocket(env.NEXT_PUBLIC_SOCKET_URL);
     socket.addEventListener("open", (e) => {
-      console.log("opened");
-      socket.send("hello from client");
       setInterval(() => {
         socket.send("stock");
       }, 5000);
@@ -25,6 +25,8 @@ export default function SocketTestPage() {
     socket.addEventListener("message", (event) => {
       console.log("Message received from server");
       console.log(event.data);
+      setMessage(event.data);
+      setTimeStamp(`${new Date()}`);
     });
   }, []);
 
@@ -32,6 +34,8 @@ export default function SocketTestPage() {
     <div className="py-24 mx-auto max-w-[400px] space-y-6">
       {/* <h1 className={pageTitleStyles}>Successfully Signed Out</h1> */}
       <p className="text-xl">This is socket test page.</p>
+      <p>{message}</p>
+      <p>{timeStamp}</p>
 
       {/* <Button asChild>
         <Link href="/sign-in">Sign In</Link>

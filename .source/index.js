0492e6c2d1661e0f89634b630c70e82518b1f1c0
jihyeonjeong11 <wjdwlgus11@gmail.com>
2025-05-11 01:36:32 +0900

    vercel?

--- .source/index.js
+++
@@ -1,7 +0,0 @@
-import { toRuntime, toRuntimeAsync } from "fumadocs-mdx"
-import * as docs_0 from "../content/docs/architecture.mdx?collection=docs&hash=94fbc73421699893a65fa70edc99609c3ddcbabe18d48387cb812f7a8b5341f9"
-import * as docs_1 from "../content/docs/component-convention.mdx?collection=docs&hash=94fbc73421699893a65fa70edc99609c3ddcbabe18d48387cb812f7a8b5341f9"
-import * as docs_2 from "../content/docs/index.mdx?collection=docs&hash=94fbc73421699893a65fa70edc99609c3ddcbabe18d48387cb812f7a8b5341f9"
-import * as meta_0 from "../content/docs/meta.json?collection=meta&hash=94fbc73421699893a65fa70edc99609c3ddcbabe18d48387cb812f7a8b5341f9"
-export const docs = [toRuntime("doc", docs_0, {"path":"architecture.mdx","absolutePath":"D:/dev/sides/stock-dashboard/Stock-dashboard/content/docs/architecture.mdx"}), toRuntime("doc", docs_1, {"path":"component-convention.mdx","absolutePath":"D:/dev/sides/stock-dashboard/Stock-dashboard/content/docs/component-convention.mdx"}), toRuntime("doc", docs_2, {"path":"index.mdx","absolutePath":"D:/dev/sides/stock-dashboard/Stock-dashboard/content/docs/index.mdx"})];
-export const meta = [toRuntime("meta", meta_0, {"path":"meta.json","absolutePath":"D:/dev/sides/stock-dashboard/Stock-dashboard/content/docs/meta.json"})];
---
+++ .source/index.ts
@@ -0,0 +1,9 @@
+// @ts-nocheck -- skip type checking
+import * as meta_0 from "../content/docs/meta.json?collection=meta&hash=1746044655900"
+import * as docs_2 from "../content/docs/index.mdx?collection=docs&hash=1746044655900"
+import * as docs_1 from "../content/docs/component-convention.mdx?collection=docs&hash=1746044655900"
+import * as docs_0 from "../content/docs/architecture.mdx?collection=docs&hash=1746044655900"
+import { _runtime } from "fumadocs-mdx"
+import * as _source from "../source.config"
+export const docs = _runtime.doc<typeof _source.docs>([{ info: {"path":"architecture.mdx","absolutePath":"D:/dev/sides/stock-dashboard/Stock-dashboard/content/docs/architecture.mdx"}, data: docs_0 }, { info: {"path":"component-convention.mdx","absolutePath":"D:/dev/sides/stock-dashboard/Stock-dashboard/content/docs/component-convention.mdx"}, data: docs_1 }, { info: {"path":"index.mdx","absolutePath":"D:/dev/sides/stock-dashboard/Stock-dashboard/content/docs/index.mdx"}, data: docs_2 }]);
+export const meta = _runtime.meta<typeof _source.meta>([{ info: {"path":"meta.json","absolutePath":"D:/dev/sides/stock-dashboard/Stock-dashboard/content/docs/meta.json"}, data: meta_0 }]);
